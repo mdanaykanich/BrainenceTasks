71e08508a2b8a54e5fceace58be8b456c9c2baac
mdanaykanich <danaykanich9@gmail.com>
2020-04-17 13:11:58 +0300

    #2: Is palindrome or not

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Task2
+{
+    class Program
+    {
+        static bool IsPalindrome(string word)
+        {
+            return String.Equals(word, new string(word.Reverse().ToArray()));
+        }
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            Console.WriteLine(IsPalindrome(s)); 
+        }
+    }
+}
